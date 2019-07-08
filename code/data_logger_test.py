defe717e5ddee7ae061a114f939c46e299e309ec
macadoodledoo <matthew.a.calder@googlemail.com>
2019-07-08 21:38:11 +0100

    Add files via upload

---
+++ code/data_logger_test.py
@@ -0,0 +1,26 @@
+import csv
+
+MARKET_epics = [\
+                "CS.D.GBPUSD.CFD.IP", "CS.D.USDJPY.CFD.IP", "CS.D.EURGBP.CFD.IP", "CS.D.EURJPY.CFD.IP", "CS.D.EURUSD.CFD.IP", "CS.D.GBPJPY.CFD.IP", \
+                "CS.D.AUDJPY.CFD.IP", "CS.D.AUDUSD.CFD.IP", "CS.D.AUDCAD.CFD.IP", "CS.D.USDCAD.CFD.IP", "CS.D.NZDUSD.CFD.IP", "CS.D.NZDJPY.CFD.IP", \
+                "CS.D.AUDEUR.CFD.IP", "CS.D.AUDGBP.CFD.IP", "CS.D.CADJPY.CFD.IP", "CS.D.NZDGBP.CFD.IP", "CS.D.NZDEUR.CFD.IP", "CS.D.NZDCAD.CFD.IP"]
+Pairs = {}
+
+#while True:
+for epic in MARKET_epics:
+    if epic[5:11] not in Pairs:
+        Pairs[epic[5:11]+" BID"] = {}
+        Pairs[epic[5:11]+" OFR"] = {}
+        Pairs[epic[5:11]+" LTP"] = {}
+        #Pairs[epic[5:11]={"BID":0,"OFR":0,"LTP";0]
+        
+with open('test.csv', "w", newline='') as csv_file:
+    writer = csv.DictWriter(csv_file,fieldnames=Pairs.keys())  
+    writer.writeheader()
+    
+    for i in range(1,10):
+        Pairs['GBPUSD BID']=i    
+        writer.writerow(Pairs)
+            
+print(Pairs)
+
