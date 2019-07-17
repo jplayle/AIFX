a3748b6924aebb2950169f090e9b5fd9cceeac3c
Matthew Calder <matthew.a.calder@googlemail.com>
2019-07-17 21:19:35 +0100

    Begin to include live logging of data metrics module

--- code/indicators.py
+++ code/indicators.py
@@ -1,10 +1,24 @@
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+from time import (clock, sleep, time)
+from datetime import datetime, timedelta
+from datetime import time as dt_time
+import email
 #import numpy as np
 
 class Indicators():
 
+    #Currencies to analyse
+    crypto_epics = ["CS.D.BITCOIN.CFD.IP", "CS.D.ETHUSD.CFD.IP", "CS.D.LTCUSD.CFD.IP", "CS.D.XRPUSD.CFD.IP"]
+	fiat_epics   = ["CS.D.GBPUSD.CFD.IP", "CS.D.USDJPY.CFD.IP", "CS.D.EURGBP.CFD.IP", "CS.D.EURJPY.CFD.IP", "CS.D.EURUSD.CFD.IP", "CS.D.GBPJPY.CFD.IP",	\
+					"CS.D.AUDJPY.CFD.IP", "CS.D.AUDUSD.CFD.IP", "CS.D.AUDCAD.CFD.IP", "CS.D.USDCAD.CFD.IP", "CS.D.NZDUSD.CFD.IP", "CS.D.NZDJPY.CFD.IP",	\
+					"CS.D.AUDEUR.CFD.IP", "CS.D.AUDGBP.CFD.IP", "CS.D.CADJPY.CFD.IP", "CS.D.NZDGBP.CFD.IP", "CS.D.NZDEUR.CFD.IP", "CS.D.NZDCAD.CFD.IP"]
+	target_epics = fiat_epics + crypto_epics
+    
+    #Indicators to document
+  	targ_fields  = ["VAL_ERR", "PERC_VAL_ERR", "DELTA_VAL_ERR", "PERC_DELTA_VAL_ERR", "STAND_DEV", "BOXPLOT", "DIRECTION"]
+    
     #Read in prediction
     df = pd.read_csv('pred.csv',sep='\t')
 
@@ -13,6 +27,47 @@ class Indicators():
     
     #Live dataset created in same way as predicted for now, will need to be live updating in future
     live = df[['DATETIME','LIVE']]
+        
+    def startup_sequence(self):
+		"""
+		1. Check if data files exist and retrieve previous update time from the last row if they do
+		2. Write necessary number of blank rows based on difference between previous time from step 1 and the account time now
+		3. For each epic data file: set self.updates_t_array[epic]['PREV'] to the last written time from step 2
+		- Because of step 3, the writer algorithm will fill in any blank rows that are required between finishing the start-up sequence and writing the first data
+		"""
+		t_now = datetime.utcnow()
+		fname_suffix = "-".join(['', str(t_now.year), str(t_now.month)]) + 'metrics' + '.csv'
+		
+		for epic in self.target_epics:
+			ccy   = epic[5:11] #currency code e.g. EURGBP
+			fname = ccy + fname_suffix
+			
+			if not path.exists(ccy):
+				makedirs(ccy)
+				
+			data_files = sorted(listdir(ccy))
+
+			if fname in data_files:
+				latest_file = ccy + '/' + fname 	
+				
+			elif data_files != []:
+				latest_file = ccy + '/' + data_files[-1]
+				
+			else:
+				self.updates_t_array[epic]['PREV'] = t_now - timedelta(minutes=self.interval_val)
+				continue
+			
+			mints = [] #missing intervals
+			with open(latest_file, 'r') as csv_rf:
+				csv_r = list(reader(csv_rf))
+				LUT   = datetime.strptime(csv_r[-1][1], '%Y-%m-%d %H:%M:%S')
+				mints = self.handle_tgap(LUT, t_now, epic)
+				
+			if mints != []:
+				data_array = [[ccy, mi] + ['' for field in self.targ_fields] for mi in mints]
+				self.updates_t_array[epic]['PREV'] = self.write_data(epic[5:11], data_array)
+			else:
+				self.updates_t_array[epic]['PREV'] = LUT - timedelta(minutes=self.interval_val)
         
     def value_accuracy(self): #Add time values in future (start/end datetime you are interested in)
         #Initialise arrays
@@ -116,6 +171,85 @@ class Indicators():
 
     # def gain_v_loss(self)
         # 
+        
+    def write_data(self, _epic_ccy, _data):
+		"""
+		- recurrent function: writes data assigning file name based on datetime of update as 'epic-year-month.csv'
+		- returns LUT (last update time) so when write_data() is called by startup_sequence() it can be used to set self.updates_t_array[epic]['PREV']
+		- schema for _data: [[epic, datetime, BID_OPEN, BID_HIGH, BID_LOW, BID_CLOSE, LTV] * n] where n >= 1
+		"""
+		row_0  = _data.pop(0)
+		t_curr = row_0[1]
+		f_year = str(t_curr.year)  #file year
+		f_mon  = str(t_curr.month) #file month
+		fname  = "-".join([_epic_ccy, f_year, f_mon]) + '.csv'
+		
+		full_path = _epic_ccy + '/' + fname
+		if not path.exists(full_path):
+			write_headers = True
+		else:
+			write_headers = False
+		
+		LUT = t_curr
+		with open(full_path, 'a') as csv_f:
+			csv_w = writer(csv_f, lineterminator='\n')
+			
+			if write_headers:
+				csv_w.writerow(['EPIC', 'DATE_TIME'] + self.targ_fields)
+			
+			csv_w.writerow(row_0)
+			
+			for r in _data[:]: # [:] ensures that _data is modified in-place by calls to _data.remove()
+				t_curr = r[1]
+				if str(t_curr.year) != f_year or str(t_curr.month) != f_mon:
+					return self.write_data(_epic_ccy, _data)
+				else:
+					csv_w.writerow(r)
+					LUT = t_curr
+					_data.remove(r)
+					
+		return LUT
+        
+        
+    def read_data(self, _epic_ccy, _data):
+		"""
+		- recurrent function: writes data assigning file name based on datetime of update as 'epic-year-month.csv'
+		- returns LUT (last update time) so when write_data() is called by startup_sequence() it can be used to set self.updates_t_array[epic]['PREV']
+		- schema for _data: [[epic, datetime, BID_OPEN, BID_HIGH, BID_LOW, BID_CLOSE, LTV] * n] where n >= 1
+		"""
+		row_0  = _data.pop(0)
+		t_curr = row_0[1]
+		f_year = str(t_curr.year)  #file year
+		f_mon  = str(t_curr.month) #file month
+		fname  = "-".join([_epic_ccy, f_year, f_mon]) + '.csv'
+		
+		full_path = _epic_ccy + '/' + fname
+		if not path.exists(full_path):
+			write_headers = True
+		else:
+			write_headers = False
+		
+		LUT = t_curr
+		with open(full_path, 'a') as csv_f:
+			csv_w = writer(csv_f, lineterminator='\n')
+			
+			if write_headers:
+				csv_w.writerow(['EPIC', 'DATE_TIME'] + self.targ_fields)
+			
+			csv_w.writerow(row_0)
+			
+			for r in _data[:]: # [:] ensures that _data is modified in-place by calls to _data.remove()
+				t_curr = r[1]
+				if str(t_curr.year) != f_year or str(t_curr.month) != f_mon:
+					return self.write_data(_epic_ccy, _data)
+				else:
+					csv_w.writerow(r)
+					LUT = t_curr
+					_data.remove(r)
+					
+		return LUT
+            
+        
 def main():
 
     indicators = Indicators()
@@ -125,5 +259,5 @@ def main():
     indicators.delta_value_accuracy()
 
     
-
-main()
+if __name__	== '__main__':
+	main()
